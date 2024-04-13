c4079a02f5eb8d207b524d20fa9154cf1be2834c
satyapriyankaponduru <106680109+satyapriyankaponduru@users.noreply.github.com>
2024-04-12 20:54:21 -0400

    Update database.py

--- database.py
+++ database.py
@@ -20,3 +20,22 @@ def insert_ticket(data):
         conn.execute("ROLLBACK")
     finally:
         conn.close()
+
+
+
+def delete_ticket(ticket_id):
+    conn = get_connection()
+    try:
+        query = text("""
+            DELETE FROM tickets
+            WHERE ticketid = :ticket_id
+        """)
+        conn.execute(query, {"ticket_id": ticket_id})  # Pass parameters as a dictionary
+        conn.commit()
+        return True
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
+        conn.rollback()
+        return False
+    finally:
+        conn.close()
