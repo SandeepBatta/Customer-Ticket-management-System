30be782d0db38f3b1925a7d517539da967580bb9
SandeepBatta <battasndp@gmail.com>
2024-04-22 00:24:10 -0400

    Streamlit main app

--- src/app.py
+++ src/app.py
@@ -1,14 +1,13 @@
 import streamlit as st
-from src.login import show_login
-from ticket import show_ticket_form, show_delete_ticket_form
+from login import show_login
+from home import home
 
 # Initialize session state
 if "logged_in" not in st.session_state:
     st.session_state["logged_in"] = False
 
 # Main app logic
-if st.session_state["logged_in"]:
-    show_ticket_form()
-    show_delete_ticket_form()
-else:
+if not st.session_state["logged_in"]:
     show_login()
+else:
+    home()
