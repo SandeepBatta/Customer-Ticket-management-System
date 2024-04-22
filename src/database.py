5958b7b0e0554c74056b59ecc78de1645465754c
SandeepBatta <battasndp@gmail.com>
2024-04-22 00:25:39 -0400

    Add database CRUD operations

--- src/database.py
+++ src/database.py
@@ -1,10 +1,11 @@
 import streamlit as st
-from sqlalchemy import create_engine, text
+import mysql.connector
+from mysql.connector import Error
 import json
 
 
 def get_connection():
-    with open("../credentials.json") as fh:
+    with open("credentials.json") as fh:
         credentials = json.load(fh)
 
     user = credentials["user"]
@@ -13,43 +14,125 @@ def get_connection():
     database = credentials["database"]
 
     # Create a connection to the MySQL database
-    engine = create_engine(f"mysql+pymysql://{user}:{password}@{hostname}/{database}")
-    return engine.connect()
-
-
-def insert_ticket(data):
-    conn = get_connection()
     try:
-        query = text(
-            """
-            INSERT INTO tickets (userid, saleid, type, subject, description, create_time, status, priority, channel)
-            VALUES (:userid, :saleid, :type, :subject, :description, :create_time, :status, :priority, :channel)
-        """
+        connection = mysql.connector.connect(
+            host=hostname,
+            database=database,
+            user=user,
+            password=password,
         )
-        conn.execute(query, data)
-        conn.commit()
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
-        conn.execute("ROLLBACK")
-    finally:
-        conn.close()
+        return connection
+    except Error as e:
+        st.error("Error connecting to database", e)
+        st.stop()
+
+
+def get_users():
+    connection = get_connection()
+    if connection.is_connected():
+        cursor = connection.cursor()
+        query = "SELECT userid, name FROM users"
+        cursor.execute(query)
+        users = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        return users
+
+
+def get_total_pages(filters, page_size):
+    connection = get_connection()
+    if connection.is_connected():
+        cursor = connection.cursor()
+
+        query = "SELECT COUNT(*) FROM tickets WHERE 1=1"
+
+        if filters["type"]:
+            query += f" AND type = '{filters['type']}'"
+
+        if filters["status"]:
+            query += f" AND status = '{filters['status']}'"
+
+        if filters["priority"]:
+            query += f" AND priority = '{filters['priority']}'"
+
+        if "user_ids" in filters and filters["user_ids"]:
+            user_ids_str = ", ".join(str(user_id) for user_id in filters["user_ids"])
+            query += f" AND userid IN ({user_ids_str})"
+
+        cursor.execute(query)
+        count = cursor.fetchone()[0]
+        cursor.close()
+        connection.close()
+
+        total_pages = (
+            (count // page_size) + 1 if count % page_size else count // page_size
+        )
+        return total_pages
+
+
+def get_tickets(filters, page_size, page_number):
+    connection = get_connection()
+    if connection.is_connected():
+        cursor = connection.cursor()
+
+        query = "SELECT t.*, u.name FROM tickets t LEFT JOIN users u ON t.userid = u.userid WHERE 1=1"
+
+        if filters["type"]:
+            query += f" AND type = '{filters['type']}'"
+
+        if filters["status"]:
+            query += f" AND status = '{filters['status']}'"
+
+        if filters["priority"]:
+            query += f" AND priority = '{filters['priority']}'"
+
+        if "user_ids" in filters and filters["user_ids"]:
+            user_ids_str = ", ".join(str(user_id) for user_id in filters["user_ids"])
+            query += f" AND t.userid IN ({user_ids_str})"
+
+        # Add pagination
+        query += f" ORDER BY create_time DESC LIMIT {page_size} OFFSET {(page_number - 1) * page_size}"
+
+        cursor.execute(query)
+        tickets = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        return tickets
+
+
+def insert_ticket(userid, subject, description, type, priority):
+    connection = get_connection()
+    if connection.is_connected():
+        cursor = connection.cursor()
+        description = description.replace('"', "'")
+        query = f"""INSERT INTO tickets (userid, type, subject, description, priority) VALUES ({userid}, '{type}', '{subject}', '{description}', '{priority}')"""
+        cursor.execute(query)
+        connection.commit()
+        cursor.close()
+        connection.close()
+        st.toast("Ticket created successfully.")
+
+
+def update_ticket(ticketid, subject, description, type, priority, status):
+    connection = get_connection()
+    if connection.is_connected():
+        cursor = connection.cursor()
+        description = description.replace('"', "'")
+        query = f"""UPDATE tickets SET subject = '{subject}', description = "{description}", priority = '{priority}', type = '{type}', status = '{status}' WHERE ticketid = {ticketid}"""
+        cursor.execute(query)
+        connection.commit()
+        cursor.close()
+        connection.close()
+        st.toast("Ticket Updated successfully.")
 
 
 def delete_ticket(ticket_id):
-    conn = get_connection()
-    try:
-        query = text(
-            """
-            DELETE FROM tickets
-            WHERE ticketid = :ticket_id
-        """
-        )
-        conn.execute(query, {"ticket_id": ticket_id})  # Pass parameters as a dictionary
-        conn.commit()
-        return True
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
-        conn.rollback()
-        return False
-    finally:
-        conn.close()
+    connection = get_connection()
+    if connection.is_connected():
+        cursor = connection.cursor()
+        query = f"""DELETE FROM tickets WHERE ticketid = {ticket_id}"""
+        cursor.execute(query)
+        connection.commit()
+        cursor.close()
+        connection.close()
+        st.toast("Ticket deleted successfully.")
