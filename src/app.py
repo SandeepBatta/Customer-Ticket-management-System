ce8b8796f7a95c431a02e8f967d92978df863f0d
SandeepBatta <battasndp@gmail.com>
2024-05-18 10:53:45 -0400

    Show database connection message

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from login import show_login, show_signup_form,signup,show_login_form
+from login import show_login, show_signup_form, signup, show_login_form
 from home import home
 
 # Initialize session state
@@ -9,14 +9,15 @@ if "logged_in" not in st.session_state:
 
 # Main app logic
 if not st.session_state["logged_in"]:
+    st.info("MySQL instance on GCP is stopped", icon="ℹ️")
+    st.stop()
     show_login()
     st.write("New user?")
     st.button("Go to Sign Up", on_click=show_signup_form)
 
-elif st.session_state["signup"] :
+elif st.session_state["signup"]:
     signup()
     st.button("Back to Login", on_click=show_login_form)
 
 else:
     home()
-
